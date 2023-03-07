2fc7e510bc1f2717cb13cfb7f1e95db1ca3e2d64
Rungrukuk <Rungrukuk@gmail.com>
2023-03-07 11:40:32 +0400

    cionfirm listing fixed

--- adminpanel/confirm-listing.php
+++ adminpanel/confirm-listing.php
@@ -8,11 +8,7 @@
         echo $id;
         $sql = "UPDATE carinfo SET status='1' WHERE id=$id";
 
-        if ($conn->query($sql) === TRUE) {
-          echo "Record updated successfully";
-        } else {
-          echo "Error updating record: " . $conn->error;
-        }
+        $conn->query($sql) === TRUE;
 
         header("Location: carinfo.php");
 ?>
